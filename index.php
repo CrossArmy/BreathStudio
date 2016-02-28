f70de3549d001990b6c6a1627eb6ae7bbe3d45d7
Szymon Radosz <Szymon Radosz>
2016-02-28 12:38:13 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      
+    <title>Breathe Studio</title>
+      
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/fontello.css" rel="stylesheet">
+      
+      <!--jquery-->
+      <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.0.min.js"></script>
+      
+    <link href='https://fonts.googleapis.com/css?family=Chivo:400,900' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+      
+  </head>
+  <body>
+    <!-- ------------ BANNER ----------->
+      
+    <div class="jumbotron banner">
+      <div class="container">
+          <div class="row nav">
+              <div class="col-sm-3 breathe">
+                <h6>Breathe</h6>
+              </div>
+              <div class="col-sm-3"></div>
+              <div class="col-sm-2"><a href="#" class="projekty_scroll">Projekty</a></div>
+              <div class="col-sm-2"><a href="#" class="zespol_scroll">Zespół</a></div>
+              <div class="col-sm-2"><a href="#" class="kontakt_scroll">Kontakt</a></div>
+          </div>
+      </div>
+        <div class="row">
+            <div class="ban"><h1>Breathe Studio</h1></div>
+        </div>
+        <div class="row">
+            <a class="down" href="#"><i class="icon-down-open"></i></a>
+        </div>
+    </div>
+      
+    <!-- ------------ PROJEKTY ----------->
+      
+    <div class="jumbotron projects" id="projekty">
+          <div class="row title">
+              <h3>PROJEKTY</h3>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 project_image">
+                <div class="projekt1">
+                    <div class="text1">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas tote bag bushwick. Pinterest flannel pork belly humblebrag, church-key pickled kickstarter.</p>
+                    </div>
+                </div>
+                <div class="projekt2">
+                    <div class="text2">
+                        <h3>Tytul2</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas tote bag bushwick. Pinterest flannel pork belly humblebrag, church-key pickled kickstarter.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 project_image">
+                <div class="projekt3">
+                    <div class="text3">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas tote bag bushwick. Pinterest flannel pork belly humblebrag, church-key pickled kickstarter.</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      
+    <!-- ------------ ZESPÓŁ ----------->
+      
+      <div class="jumbotron team" id="zespół">
+          <div class="row title">
+            <h3>ZESPÓŁ</h3>
+          </div>
+          <div class="row">
+            <div class="col-xs-12 col-sm-3 col-md-3 team_image">
+                <div class="obr1">
+                    <div class="text4">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 team_image">
+                <div class="obr2">
+                    <div class="text5">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas </p>
+                    </div>
+                </div>
+            </div>
+              <div class="col-xs-12 col-sm-3 col-md-3 team_image">
+                <div class="obr3">
+                    <div class="text6">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>Marfa biodiesel migas </p>
+                    </div>
+                  </div>
+            </div>
+              <div class="col-xs-12 col-sm-3 col-md-3 team_image">
+                <div class="obr4">
+                    <div class="text7">
+                        <h3>Tytul1</h3>
+                        <hr>
+                        <p>lorem ipsum</p>
+                    </div>
+                  </div>
+            </div>
+          </div>
+      </div>
+      
+    <!-- ------------ KONTAKT ----------->
+      
+    <div class="jumbotron social" id="kontakt">
+        <div class="row title">
+            <h3>KONTAKT</h3>
+        </div>
+        <div class="row formularz">
+            <div class="col-sm-12 col-md-6 user_form">
+                <label for="user"></label>
+                <input type="text" class="form-control" id="user" placeholder="Nazwa">
+            </div>
+            <div class="col-sm-12 col-md-6 email_form">
+                <label for="email"></label>
+                <input type="text" class="form-control" id="email" placeholder="Adres e-mail">
+            </div>
+            <div class="col-sm-12 col-md-12 message_form">
+                <label for="message"></label>
+                <input type="text" class="form-control" id="message" placeholder="Wiadomość...">
+            </div>
+            <div class="col-sm-12 col-md-12 button_class">
+                <button type="button" class="btn btn-success btn_wyslij">Wyślij!</button>
+            </div>
+        </div>
+        <div class="row icons">
+            <div class="col-sm-12 col-md-12">
+                <h2>breathe_studio@breathe.com</h2>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 social_icons">
+                <a href="#" class="icons_link"><i class="icon-facebook"></i></a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 social_icons">
+                <a href="#" class="icons_link"><i class="icon-twitter"></i></a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 social_icons">
+                <a href="#" class="icons_link"><i class="icon-instagram"></i></a>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-md-3 social_icons">
+                <a href="#" class="icons_link"><i class="icon-youtube"></i></a>
+            </div>
+        </div>
+        <div class="row footer">
+            <h6>@2016 Breathe Studio. All rights reserved</h6>
+        </div>
+    </div>
+      
+       <!-- ------------ SKRYPT ----------->
+      
+   <script>
+       $(document).ready(function(){
+
+           $( ".text1" ).hide();
+           $( ".text2" ).hide();
+           $( ".text3" ).hide();
+           
+           $( ".text4" ).hide();
+           $( ".text5" ).hide();
+           $( ".text6" ).hide();
+           $( ".text7" ).hide();
+           
+           $(".projekt1").mouseenter(function(){
+               $( ".text1" ).show();       
+           });
+           $(".projekt1").mouseleave(function(){
+               $( ".text1" ).hide();
+           });
+
+           $(".projekt2").mouseenter(function(){
+               $( ".text2" ).show();       
+           });
+           $(".projekt2").mouseleave(function(){
+               $( ".text2" ).hide();
+           });
+           
+           $(".projekt3").mouseenter(function(){
+               $( ".text3" ).show();       
+           });
+           $(".projekt3").mouseleave(function(){
+               $( ".text3" ).hide();
+           });
+           
+           $(".obr1").mouseenter(function(){
+               $( ".text4" ).show();       
+           });
+           $(".obr1").mouseleave(function(){
+               $( ".text4" ).hide();
+           });
+           
+           $(".obr2").mouseenter(function(){
+               $( ".text5" ).show();       
+           });
+           $(".obr2").mouseleave(function(){
+               $( ".text5" ).hide();
+           });
+           
+           $(".obr3").mouseenter(function(){
+               $( ".text6" ).show();       
+           });
+           $(".obr3").mouseleave(function(){
+               $( ".text6" ).hide();
+           });
+           
+           $(".obr4").mouseenter(function(){
+               $( ".text7" ).show();       
+           });
+           $(".obr4").mouseleave(function(){
+               $( ".text7" ).hide();
+           });
+           
+           $(".down").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#projekty").offset().top
+                }, 1000);
+            });
+           
+           $(".projekty_scroll").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#projekty").offset().top
+                }, 1000);
+            });
+           
+           $(".zespol_scroll").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#zespół").offset().top
+                }, 1000);
+            });
+           
+           $(".kontakt_scroll").click(function (){
+                $('html, body').animate({
+                    scrollTop: $("#kontakt").offset().top
+                }, 1000);
+            });
+        });
+        
+    </script>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
